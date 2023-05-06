b7df74ab29da3ceb875f2a4b731825a8f378a019
maxim <maxrockvardill@gmail.com>
2023-05-06 21:16:43 +0300

    add file setup.sh

---
+++ D3.2/setup.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# переходим в директорию
+cd ./terraform > /dev/null 2>&1
+
+# выполняем terraform apply без ввода yes
+terraform apply -auto-approve
+
+# ждем 60 секунд пока поднимется наш instans
+sleep 60s > /dev/null 2>&1
+
+# переходим в директорию
+cd .. && cd ./minikube-setup > /dev/null 2>&1
+
+# запускаем ansible-playbook 
+ansible-playbook play.yml --tags="docker, minikube, install_deployment"
