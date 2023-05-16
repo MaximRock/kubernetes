7c957cf3c57bdfeee6ea530ded187eaa163f57b3
MaximRock <95434302+MaximRock@users.noreply.github.com>
2023-05-16 08:04:12 +0300

    Update variables.tf

--- k8s-cluster/terraform/modules/account/variables.tf
+++ k8s-cluster/terraform/modules/account/variables.tf
@@ -3,17 +3,17 @@
 variable "yc_token" {
   type        = string
   description = "Yandex Cloud PI key"
-  default     = "y0_AgAAAABkpk0NAATuwQAAAADSwkfOmQ835bV7TKqjVbXe1ht1599AzVs"
+  default     = ""
 }
 
 variable "yc_cloud_id" {
   type        = string
   description = "Yandex Cloud id"
-  default     = "b1gfqsibs2oth9v8tqp5"
+  default     = ""
 }
 
 variable "yc_folder_id" {
   type        = string
   description = "Yndex Cloud folder id"
-  default     = "b1gt036erts2va4jvqeq"
+  default     = ""
 }
